dc4d0aa3afa41626d36da54c6adeb3e892cb2636
Philalex <philalex196401@gmail.com>
2019-07-04 22:16:15 +0200

    1.2.1
    Error in 3rd task

--- Algorithms/Algorithms/main.c
+++ Algorithms/Algorithms/main.c
@@ -35,56 +35,111 @@ int calculateSquareEquation(int a, int b, int c, float* x1, float* x2) {
     }
 }
 
-
-
+int spellAge(int age, int n) {
+    n = age%10;
+    if (n == 1 && (age != 11 && age !=111)) {
+        
+        printf("%d 1\n", age);
+        return 1;
+        
+    } else if (n >=2 && n <= 4 && (age < 5 || age > 114)) {
+        
+       printf("%d 2\n", age);
+        return 2;
+        
+    } else {
+        
+        printf("%d 5\n", age);
+        return 5;
+    }
+}
 
 
 int main(int argc, const char * argv[]) {
     // 1. Рассчитать и вывести индекс массы тела по формуле I = m / (h * h), где m – масса тела в килограммах, h – рост в метрах.
     
-    float i, m, h;
-
-    printf("This program will calculate Body Mass Index (BMI)");
-    printf("\n Enter your weight: ");
-    scanf("%f", &m);
-    printf("\n Enter your height: ");
-    scanf("%f", &h);
-
-    i = m / (h * h);
-    printf("Your BMI equals: %.2f", i);
-    printf("\n");
+//    float i, m, h;
+//
+//    printf("This program will calculate Body Mass Index (BMI)");
+//    printf("\n Enter your weight: ");
+//    scanf("%f", &m);
+//    printf("\n Enter your height: ");
+//    scanf("%f", &h);
+//
+//    i = m / (h * h);
+//    printf("Your BMI equals: %.2f", i);
+//    printf("\n");
+//
+//
+//    // 2. Написать программу нахождения корней заданного квадратного уравнения (ax2 + bx + c = 0).
+//
+//    float a, b, c, x1, x2;
+//
+//    printf("This program will calculate a square equation 'ax2 + bx + c = 0'");
+//    printf("\n Enter a: ");
+//    scanf("%f", &a);
+//    printf("\n Enter b: ");
+//    scanf("%f", &b);
+//    printf("\n Enter c: ");
+//    scanf("%f", &c);
+//
+//    int result;
+//    result = calculateSquareEquation(a, b, c, &x1, &x2);
+//    switch (result) {
+//        case 0:
+//            printf("Equation has a unique root x = %f", x1);
+//            break;
+//        case 1:
+//            printf("Equation has two roots x1 = %f, x2 = %f", x1, x2);
+//            break;
+//        case -1:
+//            printf("Equation has no roots");
+//            break;
+//        default:
+//            printf("Exceptional answer: %d", result);
+//
+//    }
     
     
-    // 2. Написать программу нахождения корней заданного квадратного уравнения (ax2 + bx + c = 0).
     
-    float a, b, c, x1, x2;
+// 3. Ввести возраст человека (от 1 до 150 лет) и вывести его вместе со словом «год», «года» или «лет».
     
-    printf("This program will calculate a square equation 'ax2 + bx + c = 0'");
-    printf("\n Enter a: ");
-    scanf("%f", &a);
-    printf("\n Enter b: ");
-    scanf("%f", &b);
-    printf("\n Enter c: ");
-    scanf("%f", &c);
+    int age;
+    int n;
     
-    int result;
-    result = calculateSquareEquation(a, b, c, &x1, &x2);
-    switch (result) {
-        case 0:
-            printf("Equation has a unique root x = %f", x1);
-            break;
-        case 1:
-            printf("Equation has two roots x1 = %f, x2 = %f", x1, x2);
-            break;
-        case -1:
-            printf("Equation has no roots");
-            break;
-        default:
-            printf("Exceptional answer: %d", result);
-            
-            
+    printf("\n Введите ваш возраст цифрами: ");
+    scanf("%d", &age);
+   
+    int enteredAge = spellAge(age, n);
+    if (age > 0 && age <= 150) {
+        switch (enteredAge) {
+            case 1:
+                printf("Ваш возраст - %d год.", age);
+            case 2:
+                printf("Ваш возраст - %d годa.", age);
+            case 5:
+                printf("Ваш возраст - %d лет.", age);
+//            default:
+//                printf("Ошибочка вышла.");
+//                printf("\n");
+//        } else {
+//            printf("Люди столько не живут.")
+        }
     }
+
+    
+ 
+    
+    
+ // 4. Даны числовые координаты двух полей шахматной доски (x1, y1, x2, y2). Требуется определить, относятся ли к поля к одному цвету или нет.
+    
+    
+    
+    
+    
+    
             printf("\n");
 
             return 0;
 }
+
