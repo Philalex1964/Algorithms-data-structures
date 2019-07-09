32a61c2245004abb9b415f794cb04f68170e23ca
Philalex <philalex196401@gmail.com>
2019-07-09 12:03:25 +0200

    2.0.0
    Lesson2 Hometask

---
+++ Algorithms_Lesson2/Algorithms_Lesson2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithms_Lesson2
+//
+//  Created by Alexander Filippov on 7/9/19.
+//  Copyright © 2019 Alexander Filippov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
