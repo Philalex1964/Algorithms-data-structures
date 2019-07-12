de34cf3696fb0638caaf6883a3339c2c1e2e3e9d
Philalex <philalex196401@gmail.com>
2019-07-12 11:56:43 +0200

    3.0.0
    Lesson3 Homework

---
+++ Algorithms_Lesson3/Algorithms_Lesson3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithms_Lesson3
+//
+//  Created by Alexander Filippov on 7/12/19.
+//  Copyright © 2019 Alexander Filippov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
