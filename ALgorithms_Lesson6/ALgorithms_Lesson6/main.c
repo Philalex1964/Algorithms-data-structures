28cdb5d12c90647e455ff0cd0db4b0ba0016a58b
Philalex <philalex196401@gmail.com>
2019-07-23 12:07:53 +0200

    6.0.1

---
+++ ALgorithms_Lesson6/ALgorithms_Lesson6/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ALgorithms_Lesson6
+//
+//  Created by Alexander Filippov on 7/23/19.
+//  Copyright © 2019 Alexander Filippov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
